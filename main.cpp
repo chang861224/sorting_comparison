7bd6e0a7e6fc9909b23e01a412a3fe9a987a50e1
Chi-Hung Chang <chihung861224@gmail.com>
2021-10-14 02:30:24 +0800

    fix method var

--- main.cpp
+++ main.cpp
@@ -14,14 +14,13 @@ int main(int argc, char** argv){
     clock_t start, end;
     int index = 10;
     char* method = new char[20];
-    strcpy(method, "BubbleSort");
 
     if(argc == 1){
         cout << "Options Description:" << endl;
         cout << "\t-index <int>" << endl;
         cout << "\t\tThe index based on 2, which means the length of the number array. Default is 10." << endl;
         cout << "\t-method <string>" << endl;
-        cout << "\t\tThe sorting method. Default is \"bubble sort\"" << endl;
+        cout << "\t\tThe sorting method. Required." << endl;
         cout << "\t\tHere is the method options:" << endl;
         cout << "\t\t1)BubbleSort 2)InsertionSort 3)MergeSort 4)HeapSort 5)DutchQuickSort 6)LomutoQuickSort 7)HoareQuickSort" << endl;
 
